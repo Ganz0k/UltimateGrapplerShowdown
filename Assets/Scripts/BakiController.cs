ce7c95fa29d02b701a190a045ef3913916e44016
Ganz0k <luisgonzalocervantes@gmail.com>
2023-10-10 21:02:57 -0700

    still working on the scripts

--- Assets/Scripts/BakiController.cs
+++ Assets/Scripts/BakiController.cs
@@ -1,10 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class BakiController : MonoBehaviour {
     
     private Animator animator;
+    private Transform personaje;
+    private const int InputBufferSize = 10;
+    private KeyCode[] inputBuffer = new KeyCode[InputBufferSize];
+    private int inputIndex = 0;
+
+    public float inputTimeout = 0.5f;
+    public KeyCode[] punchKeys = {
+        KeyCode.U,
+        KeyCode.I,
+        KeyCode.O
+    };
+    public KeyCode[] kickKeys = {
+        KeyCode.J,
+        KeyCode.K,
+        KeyCode.L
+    };
+    public keyCode[] quarterCircleBackLeft = { //Same as quarterCircleForwardRight
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A
+    };
+    public KeyCode[] quarterCircleBackRight = { //Same as quarterCircleForwardLeft
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D
+    };
+    public KeyCode[] downDown = {
+        KeyCode.S,
+        KeyCode.S
+    };
+    public KeyCode[] doubleQuarterCircleForwardLeft = {
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D
+    };
+    public KeyCode[] doubleQuarterCircleForwardRight = {
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A
+    };
+
+    private float lastInputTime;
     
     // Start is called before the first frame update
     void Start() {
@@ -15,8 +69,213 @@ public class BakiController : MonoBehaviour {
     void Update() {
         bool isMovingForwards = Input.GetKey(KeyCode.D);
         bool isMovingBackwards = Input.GetKey(KeyCode.A);
+        bool isJumping = Input.GetKey(KeyCode.W);
+        bool lightPunch = Input.GetKeyDown(KeyCode.U);
+        bool mediumPunch = Input.GetKeyDown(KeyCode.I);
+        bool heavyPunch = Input.GetKeyDown(KeyCode.O);
+        bool lightKick = Input.GetKeyDown(KeyCode.J);
+        bool mediumKick = Input.GetKeyDown(KeyCode.K);
+        bool heavyKick = Input.GetKeyDown(KeyCode.L);
 
-        animator.SetBool("D_key pressed", isMovingForwards);
-        animator.SetBool("A_key pressed", isMovingBackwards);
+        animator.SetBool("Forward pressed", isMovingForwards);
+        animator.SetBool("Back pressed", isMovingBackwards);
+        animator.SetBool("Up pressed", isJumping);
+
+        
+
+        if (Time.time - lastInputTime > inputTimeout) {
+            inputIndex = 0;
+        }
+
+        if (inputIndex < InputBufferSize) {
+            if (Input.GetKeyDown(quarterCircleBackLeft[inputIndex])) {
+                inputBuffer[inputIndex] = quarterCircleBackLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+            
+            if (Input.GetKeyDown(quarterCircleBackRight[inputIndex])) {
+                inputBuffer[inputIndex] = quarterCircleBackLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(downDown[inputIndex])) {
+                inputBuffer[inputIndex] = downDown[inputIndex];
+                inputIndex ++;
+            }
+
+            if (Input.GetKeyDown(doubleQuarterCircleForwardLeft[inputIndex])) {
+                inputBuffer[inputIndex] = doubleQuarterCircleForwardLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(doubleQuarterCircleForwardRight[inputIndex])) {
+                inputBuffer[inputIndex] = doubleQuarterCircleForwardRight[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+        }
+
+        if (ContainsQuarterCircleBackLeft()) {
+            if (Array.Exists(kickKeys, key => Input.GetKeyDown(key))) {
+                TriggerCockroachDash();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsQuarterCircleBackLeft()) {
+            if (Array.Exists(kickKeys, key => Input.GetKeyDown(key))) {
+                TriggerCockroachDash();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsQuarterCircleBackLeft()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerWhipStrike();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsQuarterCircleBackRight()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerWhipStrike();
+            }
+
+            inputIndex = 0;
+        }
+        
+        if (ContainsDownDown()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerDemonBack();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsDoubleQuarterCircleForwardLeft()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerTriceratopsFist();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsDoubleQuarterCircleForwardRight()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerTriceratopsFist();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (lightPunch) {
+            animator.SetTrigger("LightPunchTrigger");
+        }
+
+        if (mediumPunch) {
+            animator.SetTrigger("MediumPunchTrigger");
+        }
+
+        if (heavyPunch) {
+            animator.SetTrigger("HeavyPunchTrigger");
+        }
+
+        if (lightKick) {
+            animator.SetTrigger("LightKickTrigger");
+        }
+
+        if (mediumKick) {
+            animator.SetTrigger("MediumKickTrigger");
+        }
+
+        if (heavyKick) {
+            animator.SetTrigger("HeavyKickTrigger");
+        }
+    }
+
+    void TriggerCockroachDash() {
+        animator.SetTrigger("CockroachDashTrigger");
+    }
+
+    void TriggerWhipStrike() {
+        animator.SetTrigger("WhipStrikeTrigger");
+    }
+
+    void TriggerDemonBack() {
+        animator.SetTrigger("DemonBackTrigger");
+    }
+
+    void TriggerTriceratopsFist() {
+        animator.SetTrigger("TriceratopsFistTrigger");
+    }
+
+    void TriggerTriceratopsFistFollowup() {
+        animator.SetTrigger("TriceratopsFistFollowupTrigger");
+    }
+
+    bool ContainsQuarterCircleBackLeft() {
+        for (int i = 0; i <= inputBuffer.Length - quarterCircleBackLeft.Length: i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(quarterCircleBackLeft.Length).ToArray();
+
+            if (subsequence.SequenceEqual(quarterCircleBackLeft)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsQuarterCircleBackRight() {
+        for (int i = 0; i <= inputBuffer.Length - quarterCircleBackRight.Length: i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(quarterCircleBackRight.Length).ToArray();
+
+            if (subsequence.SequenceEqual(quarterCircleBackRight)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDownDown() {
+        for (int i = 0; i <= inputBuffer.Length - downDown.Length: i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(downDown.Length).ToArray();
+
+            if (subsequence.SequenceEqual(downDown)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDoubleQuarterCircleForwardLeft() {
+        for (int i = 0; i <= inputBuffer.Length - doubleQuarterCircleForwardLeft.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(doubleQuarterCircleForwardLeft.Length).ToArray();
+
+            if (subsequence.SequenceEqual(doubleQuarterCircleForwardLeft)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDoubleQuarterCircleForwardRight() {
+        for (int i = 0; i <= inputBuffer.Length - doubleQuarterCircleForwardRight.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(doubleQuarterCircleForwardRight.Length).ToArray();
+
+            if (subsequence.SequenceEqual(doubleQuarterCircleForwardRight)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- Assets/Scripts/YujiroController.cs
+++ Assets/Scripts/YujiroController.cs
@@ -1,18 +1,369 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
-public class YujiroController : MonoBehaviour
-{
+public class YujiroController : MonoBehaviour {
+    
+    private Animator animator;
+    private Transform personaje;
+    private const int InputBufferSize = 10;
+    private KeyCode[] inputBuffer = new KeyCode[InputBufferSize];
+    private int inputIndex = 0;
+
+    public float inputTimeout = 0.5f;
+    public KeyCode[] punchKeys = {
+        KeyCode.U,
+        KeyCode.I,
+        KeyCode.O
+    };
+    public KeyCode[] kickKeys = {
+        KeyCode.J,
+        KeyCode.K,
+        KeyCode.L
+    };
+    public KeyCode[] quarterCircleForwardLeft = {
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D
+    };
+    public KeyCode[] quarterCircleForwardRight = {
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A
+    };
+    public KeyCode[] dragonPunchLeft = {
+        KeyCode.D,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D
+    };
+    public KeyCode[] dragonPunchRight = {
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A
+    };
+    public KeyCode[] halfCircleBackLeft = {
+        KeyCode.D,
+        KeyCode.D,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A
+    };
+    public KeyCode[] halfCircleBackRight = {
+        KeyCode.A,
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D
+    };
+    public KeyCode[] doubleQuarterCircleForwardLeft = {
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.D
+    };
+    public KeyCode[] doubleQuarterCircleForwardRight = {
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.S,
+        KeyCode.A,
+        KeyCode.A
+    };
+
+    private float lastInputTime;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        animator = GetComponent<Animator>();
+        personaje = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        bool isMovingForwards = Input.GetKey(KeyCode.D);
+        bool isMovingBackwards = Input.GetKey(KeyCode.A);
+        bool isJumping = Input.GetKey(KeyCode.W);
+        bool lightPunch = Input.GetKeyDown(KeyCode.U);
+        bool mediumPunch = Input.GetKeyDown(KeyCode.I);
+        bool heavyPunch = Input.GetKeyDown(KeyCode.O);
+        bool lightKick = Input.GetKeyDown(KeyCode.J);
+        bool mediumKick = Input.GetKeyDown(KeyCode.K);
+        bool heavyKick = Input.GetKeyDown(KeyCode.L);
+
+        animator.SetBool("Forward pressed", isMovingForwards);
+        animator.SetBool("Back pressed", isMovingBackwards);
+        animator.SetBool("Up pressed", isJumping);
+
+        if (Time.time - lastInputTime > inputTimeout) {
+            inputIndex = 0;
+        }
+
+        if (inputIndex < InputBufferSize) {
+            if (Input.GetKeyDown(quarterCircleForwardLeft[inputIndex])) {
+                inputBuffer[inputIndex] = quarterCircleForwardLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(quarterCircleForwardRight[inputIndex])) {
+                inputBuffer[inputIndex] = quarterCircleForwardRight[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(dragonPunchLeft[inputIndex])) {
+                inputBuffer[inputIndex] = dragonPunchLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+            
+            if (Input.GetKeyDown(dragonPunchRight[inputIndex])) {
+                inputBuffer[inputIndex] = dragonPunchRight[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(halfCircleBackLeft[inputIndex])) {
+                inputBuffer[inputIndex] = halfCircleBackLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(halfCircleBackRight[inputIndex])) {
+                inputBuffer[inputIndex] = halfCircleBackRight[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(doubleQuarterCircleForwardLeft[inputIndex])) {
+                inputBuffer[inputIndex] = doubleQuarterCircleForwardLeft[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+
+            if (Input.GetKeyDown(doubleQuarterCircleForwardRight[inputIndex])) {
+                inputBuffer[inputIndex] = doubleQuarterCircleForwardRight[inputIndex];
+                inputIndex ++;
+                lastInputTime = Time.time;
+            }
+        }
+
+        if (ContainsQuarterCircleForwardLeft()) {
+            if (Array.Exists(kickKeys, key => Input.GetKeyDown(key))) {
+                TriggerOgreAxeKick();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsQuarterCircleForwardRight()) {
+            if (Array.Exists(kickKeys, key => Input.GetKeyDown(key))) {
+                TriggerOgreAxeKick();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsDragonPunchLeft()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerOgreUppercut();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsDragonPunchRight()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerOgreUppercut();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsHalfCircleBackLeft()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerOgreShaori();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsHalfCircleBackRight()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerOgreShaori();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (ContainsDoubleQuarterCircleForwardLeft()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerHugOfDeath();
+            }
+
+            inputIndex = 0;
+        }
         
+        if (ContainsDoubleQuarterCircleForwardRight()) {
+            if (Array.Exists(punchKeys, key => Input.GetKeyDown(key))) {
+                TriggerHugOfDeath();
+            }
+
+            inputIndex = 0;
+        }
+
+        if (lightPunch) {
+            animator.SetTrigger("LightPunchTrigger");
+        }
+
+        if (mediumPunch) {
+            animator.SetTrigger("MediumPunchTrigger");
+        }
+
+        if (heavyPunch) {
+            animator.SetTrigger("HeavyPunchTrigger");
+        }
+
+        if (lightKick) {
+            animator.SetTrigger("LightKickTrigger");
+        }
+
+        if (mediumKick) {
+            animator.SetTrigger("MediumKickTrigger");
+        }
+
+        if (heavyKick) {
+            animator.SetTrigger("HeavyKickTrigger");
+        }
+    }
+
+    void TriggerOgreAxeKick() {
+        animator.SetTrigger("OgreAxeKickTrigger");
+    }
+
+    void TriggerOgreUppercut() {
+        animator.SetTrigger("OgreUppercutTrigger");
+    }
+
+    void TriggerOgreShaori() {
+        animator.SetTrigger("OgreShaoriTrigger");
+    }
+
+    void TriggerHugOfDeath() {
+        animator.SetTrigger("HugOfDeathTrigger");
+    }
+
+    void TriggerHugOfDeathFollowup() {
+        animator.SetTrigger("HugOfDeathFollowupTrigger");
+    }
+
+    bool ContainsQuarterCircleForwardLeft() {
+        for (int i = 0; i <= inputBuffer.Length - quarterCircleForwardLeft.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(quarterCircleForwardLeft.Length).ToArray();
+
+            if (subsequence.SequenceEqual(quarterCircleForwardLeft)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsQuarterCircleForwardRight() {
+        for (int i = 0; i <= inputBuffer.Length - quarterCircleForwardRight.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(quarterCircleForwardRight.Length).ToArray();
+
+            if (subsequence.SequenceEqual(quarterCircleForwardRight)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDragonPunchLeft() {
+        for (int i = 0; i <= inputBuffer.Length - dragonPunchLeft.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(dragonPunchLeft.Length).ToArray();
+
+            if (subsequence.SequenceEqual(dragonPunchLeft)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDragonPunchRight() {
+        for (int i = 0; i <= inputBuffer.Length - dragonPunchRight.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(dragonPunchRight.Length).ToArray();
+
+            if (subsequence.SequenceEqual(dragonPunchRight)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsHalfCircleBackLeft() {
+        for (int i = 0; i <= inputBuffer.Length - halfCircleBackLeft.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(halfCircleBackLeft.Length).ToArray();
+
+            if (subsequence.SequenceEqual(halfCircleBackLeft)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsHalfCircleBackRight() {
+        for (int i = 0; i <= inputBuffer.Length - halfCircleBackRight.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(halfCircleBackRight.Length).ToArray();
+
+            if (subsequence.SequenceEqual(halfCircleBackRight)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDoubleQuarterCircleForwardLeft() {
+        for (int i = 0; i <= inputBuffer.Length - doubleQuarterCircleForwardLeft.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(doubleQuarterCircleForwardLeft.Length).ToArray();
+
+            if (subsequence.SequenceEqual(doubleQuarterCircleForwardLeft)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ContainsDoubleQuarterCircleForwardRight() {
+        for (int i = 0; i <= inputBuffer.Length - doubleQuarterCircleForwardRight.Length; i ++) {
+            KeyCode[] subsequence = inputBuffer.Skip(i).Take(doubleQuarterCircleForwardRight.Length).ToArray();
+
+            if (subsequence.SequenceEqual(doubleQuarterCircleForwardRight)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
