6600e5d54a916b3dccc9d4532631b53dfa5135ad
Saul SW <101837205+Sulowapo@users.noreply.github.com>
2023-10-24 15:51:03 -0700

    Script camara prueba

---
+++ Assets/Scripts/camaraPelea.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraPelea : MonoBehaviour
+{
+    public Transform target1; // Primer GameObject a seguir
+    public Transform target2; // Segundo GameObject a seguir
+    public Vector3 offset;   // Desplazamiento de la cámara
+
+    [Range(0.1f, 10.0f)]
+    public float smoothness = 2.0f; // Suavidad de seguimiento
+
+    private Vector3 midpoint;
+
+    void Start()
+    {
+        midpoint = GetMidpoint();
+    }
+
+    void Update()
+    {
+        midpoint = GetMidpoint();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = midpoint + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothness * Time.deltaTime);
+    }
+
+    Vector3 GetMidpoint()
+    {
+        Vector3 midpointPosition = (target1.position + target2.position) / 2f;
+        return new Vector3(midpointPosition.x, midpointPosition.y, transform.position.z);
+    }
+}
